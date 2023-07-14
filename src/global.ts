b413887828bf0e61bca360ea732efa78dc616225
Stpehen Burgos <stephen.burgos782004@gmail.com>
2023-07-14 14:19:20 -0500

    Export globals

---
+++ src/global.ts
@@ -0,0 +1,29 @@
+declare global {
+  export interface KeyValueAny {
+    [key: string]: any;
+  }
+  export interface KeyValueTyped<T = string> {
+    [key: string]: T;
+  }
+
+  export interface Window {
+    SARDIUSENDPOINT: string;
+  }
+
+
+  export type ObjectKeys<T> = T extends object
+    ? (keyof T)[]
+    : T extends number
+    ? []
+    : T extends Array<any> | string
+    ? string[]
+    : never;
+
+  export interface ObjectConstructor {
+    keys<T>(o: T): ObjectKeys<T>;
+  }
+
+  export type TargetElement = Element | undefined | null;
+}
+
+export {};
