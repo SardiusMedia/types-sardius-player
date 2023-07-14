8ae8d573c5b5a4285df1a52671a7c82ecd953a58
Stpehen Burgos <stephen.burgos782004@gmail.com>
2023-07-14 14:13:53 -0500

    Add keyvalue globasl

--- src/keyValuePair.ts
+++ src/keyValuePair.ts
@@ -1,6 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface KeyValueAny {
   [key: string]: any;
 }
 export interface KeyValueTyped<T = string> {
   [key: string]: T;
 }
+
+declare global {
+  export interface KeyValueAny {
+    [key: string]: any;
+  }
+  export interface KeyValueTyped<T = string> {
+    [key: string]: T;
+  }
+}
+
+export {};
