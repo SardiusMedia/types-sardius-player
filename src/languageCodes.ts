a94ffbd858d6a38b3ad6069701cbeebbb40df875
Stpehen Burgos <stephen.burgos782004@gmail.com>
2023-08-30 13:19:46 -0500

    Update languageCodes.ts

--- src/languageCodes.ts
+++ src/languageCodes.ts
@@ -25,27 +25,49 @@ const languageCodes = [
 
 const languageCodesUppercase = [
   'XZ', // American Sign Language
+  'XZCC', // American Sign Language
   'AM', // Amharic
+  'AMCC', // Amharic
   'AR', // Arabic
+  'ARCC', // Arabic
   'CA', // Cantonese
+  'CACC', // Cantonese
   'DE', // German
+  'DECC', // German
   'EN', // English
+  'ENCC', // English
   'ES', // Spanish
+  'ESCC', // Spanish
   'FR', // French
+  'FRCC', // French
   'HI', // Hindi
+  'HICC', // Hindi
   'ID', // Indonesian
+  'IDCC', // Indonesian
   'IT', // Italian
+  'ITCC', // Italian
   'JA', // Japanese
+  'JACC', // Japanese
   'KO', // Korean
+  'KOCC', // Korean
   'MN', // Mongolian
+  'MNCC', // Mongolian
   'MO', // Mongolian
+  'MOCC', // Mongolian
   'NL', // Dutch
+  'NLCC', // Dutch
   'PL', // Polish
+  'PLCC', // Polish
   'PT', // Portuguese
+  'PTCC', // Portuguese
   'RU', // Russian
+  'RUCC', // Russian
   'TH', // Thai
+  'THCC', // Thai
   'XZ', // ASL
+  'XZCC', // ASL
   'ZH', // Mandarin
+  'ZHCC', // Mandarin
 ] as const;
 
 export type LanguageCode = (typeof languageCodes)[number];
