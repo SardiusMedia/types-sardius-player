ce3dabd792520acaf68a4d63c9957f7e6394f4a0
Stpehen Burgos <stephen.burgos782004@gmail.com>
2023-09-08 13:15:52 -0500

    Model update

--- src/videojs.ts
+++ src/videojs.ts
@@ -14,7 +14,7 @@ import {
   LanguageCodesUppercase,
   LanguageCodesUppercaseCC,
 } from './languageCodes';
-import { YouboraAdapter } from './youbora';
+import { YouboraPlugin } from './youbora';
 
 export type VJSTech = videojs.Tech;
 export type TextTrackOptions = videojs.TextTrackOptions;
@@ -191,7 +191,7 @@ export interface SJSPlayer extends VJSPlayer {
   playerManager: PlayerManagerClass;
   options_: videojs.PlayerOptions & { youtube: Youtube };
   id_?: string;
-  youboraplugin?: YouboraAdapter<SJSPlayer>;
+  youboraplugin?: YouboraPlugin<SJSPlayer>;
 }
 
 interface SardiusControlBar extends videojs.ControlBar {
