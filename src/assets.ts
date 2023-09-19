f6434506c7e2d1fc6fe13875b1e3d3828dd4f5fc
Stpehen Burgos <stephen.burgos782004@gmail.com>
2023-09-19 11:47:20 -0500

    Update assets.ts

--- src/assets.ts
+++ src/assets.ts
@@ -45,7 +45,7 @@ export interface AssetFile {
   width?: number | null;
 }
 
-export interface AssetResponseMetadata {
+interface AssetResponseMetadata {
   duration: number;
   genres?: string[];
   isPreview?: boolean;
