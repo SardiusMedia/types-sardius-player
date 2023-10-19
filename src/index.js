ad4d9cfbde3161b190a140c6057aed90446aa0f2
Stpehen Burgos <stephen.burgos782004@gmail.com>
2023-10-19 12:07:21 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import events from './events';
+
+export * from './accounts';
+export * from './assets';
+export * from './common';
+export * from './dash';
+export * from './db';
+export * from './feeds';
+export * from './global';
+export * from './languageCodes';
+export * from './mpx';
+export * from './player';
+export * from './req';
+export * from './res';
+export * from './sardius';
+export * from './videojs';
+export * from './youbora';
+export * from './youtube';
+export * from './events';
+
+export { events };
