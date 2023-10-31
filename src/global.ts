5a0fc38c8996f813422bf6e1c690ff588e958866
Stpehen Burgos <stephen.burgos782004@gmail.com>
2023-10-31 14:30:27 -0500

    Update global.ts

--- src/global.ts
+++ src/global.ts
@@ -17,6 +17,18 @@ declare global {
 
   export const VJSType: typeof VJS;
 
+  export type ObjectKeys<T> = T extends object
+    ? (keyof T)[]
+    : T extends number
+    ? []
+    : T extends Array<any> | string
+    ? string[]
+    : never;
+
+  export interface ObjectConstructor {
+    keys<T>(o: T): ObjectKeys<T>;
+  }
+
   export type BasicObjectTypes = string | number | boolean | undefined;
 }
 
